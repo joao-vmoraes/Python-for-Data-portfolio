87c7347eb59e81da59b39a6e022c92a940381022
João Moraes <jvmmoraesdev@gmail.com>
2026-02-12 02:14:17 -0300

    feat: Create table customers and insert values

--- python/test_mysql.py
+++ python/test_mysql.py
@@ -1,15 +1,30 @@
-import mysql.connector
+import sqlite3
+from pathlib import Path
 
-con = mysql.connector.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="root",
-    database="dados_db"
-)
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
 
+con = sqlite3.connect(DB_FILE)
 cursor = con.cursor()
-cursor.execute("SELECT DATABASE();")
 
-for x in cursor:
-    print(x)
+# cria a tabelka
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS customers'
+    '(' 
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT, '
+    'weight REAL'
+    ')'
+)
+con.commit()
+
+#registrar valores nas colunas da tabela
+cursor.execute(
+    'INSERT INTO customers (id, name, weight)'
+    'VALUES (NULL, "Joao Vitor", 70)'
+)
+con.commit()
+
+cursor.close()
+con.close()
