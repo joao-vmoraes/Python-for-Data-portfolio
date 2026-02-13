aba77ddf557d78d57675a3493b4084f95025d2dc
João Moraes <jvmmoraesdev@gmail.com>
2026-02-13 01:13:30 -0300

    feat(pymyslq): CREATE table customers

---
+++ python_with_mysql/001_create_table_customers.py
@@ -0,0 +1,25 @@
+import os
+import pymysql
+import dotenv
+
+dotenv.load_dotenv('..')
+
+conn = pymysql.connect(
+    host= os.environ['MYSQL_HOST'],
+    user= os.environ['MYSQL_USER'],
+    password= os.environ['MYSQL_PASSWORD'],
+    database= os.environ['MYSQL_DATABASE'],
+)
+cursor = conn.cursor()
+
+with conn:
+    with conn.cursor() as cursor:
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS customers ('
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'nome VARCHAR(30) NOT NULL, '
+            'idade INT NOT NULL, '
+            'PRIMARY KEY (id) '
+            ') '
+        )
+        conn.commit()
